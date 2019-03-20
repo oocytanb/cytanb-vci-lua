f5a9f069a54a84b56c9daaba55ff06e26b81e5ad
oO <47592289+oocytanb@users.noreply.github.com>
2019-03-20 19:00:00 +0900

    Add: VRM and VCI packages.

---
+++ Assets/VCI/UniVCI/Scripts/Format/VCISpecVersion.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable
+using System;
+
+namespace VCI
+{
+    public class VCISpecVersion
+    {
+        public const int Major = 0;
+
+        //public const int Minor = 10;
+
+        // 当面UniVCIのバージョンと同じ
+        public const int Minor = VCIVersion.MINOR;
+
+        public static string Version
+        {
+            get
+            {
+                return String.Format("{0}.{1}", Major, Minor);
+            }
+        }
+    }
+}
