352b79ae534f42108d52f0652fd6e64a8821bc4b
oO <47592289+oocytanb@users.noreply.github.com>
2019-06-14 00:20:06 +0900

    Add json to vci mock

--- src/cytanb_vci_mock.lua
+++ src/cytanb_vci_mock.lua
@@ -3,15 +3,17 @@
 --  MIT Licensed
 ----------------------------------------------------------------
 
+-- VCI 環境の簡易 Mock モジュール。
+
 if not string.contains then
-	-- [MoonSharp](http://www.moonsharp.org/additions.html)
+	-- [MoonSharp](https://www.moonsharp.org/additions.html)
 	string.contains = function (str1, str2)
 		return string.find(str1, str2, 1, true) ~= nil
 	end
 end
 
 if not string.startsWith then
-	-- [MoonSharp](http://www.moonsharp.org/additions.html)
+	-- [MoonSharp](https://www.moonsharp.org/additions.html)
 	string.startsWith = function (str1, str2)
 		local len1 = string.len(str1)
 		local len2 = string.len(str2)
@@ -26,7 +28,7 @@ if not string.startsWith then
 end
 
 if not string.endsWith then
-	-- [MoonSharp](http://www.moonsharp.org/additions.html)
+	-- [MoonSharp](https://www.moonsharp.org/additions.html)
 	string.endsWith = function (str1, str2)
 		local len1 = string.len(str1)
 		local len2 = string.len(str2)
@@ -39,4 +41,30 @@ if not string.endsWith then
 		local i = string.find(str1, str2, - len2, true)
 		return i ~= nil
 	end
+end
+
+if not json then
+	-- [MoonSharp](https://www.moonsharp.org/additions.html)
+	local dkjson = require('dkjson')
+	json = {
+		parse = function (jsonString)
+			return dkjson.decode(jsonString, 1, dkjson.null)
+		end,
+
+		serialize = function (table)
+			local t = type(table)
+			if t ~= 'table' then
+				error('Invalid type: ' .. t)
+			end
+			return dkjson.encode(table)
+		end,
+
+		isNull = function (val)
+			return val == dkjson.null
+		end,
+
+		null = function ()
+			return dkjson.null
+		end
+	}
 end
--- src/cytanb_vci_mock_spec.lua
+++ src/cytanb_vci_mock_spec.lua
@@ -8,30 +8,6 @@ describe('Test cytanb_vci_mock', function ()
 		require('cytanb_vci_mock')
 	end)
 
-	teardown(function ()
-		string.contains = nil
-		string.startsWith = nil
-		string.endsWith = nil
-
-		local globals = {
-			"_MOONSHARP",
-			"dynamic",
-			"json",
-			"vci",
-			"Vector2",
-			"Vector3",
-			"Vector4",
-			"Color",
-			"Quaternion",
-			"Matrix4x4",
-			"TimeSpan"
-		}
-
-		for key, val in pairs(globals) do
-			_G[val] = nil
-		end
-	end)
-
 	it('string.contains', function ()
 		assert.is_true(string.contains('abcdefg', 'ab'))
 		assert.is_true(string.contains('abcdefg', 'cde'))
@@ -55,5 +31,29 @@ describe('Test cytanb_vci_mock', function ()
 		assert.is_true(string.endsWith('abcdefg', 'efg'))
 		assert.is_false(string.endsWith('abcdefg', 'ef'))
 		assert.is_false(string.endsWith('abcdefg', 'zabcdefg'))
+	end)
+
+	it('json', function ()
+		local table1 = {foo = "apple"}
+		local table2 = {bar = 1234.5}
+		local table3 = {baz = json.null()}
+		local table4 = {qux = true, quux = table1}
+
+		local jstr1 = json.serialize(table1)
+		local jstr2 = json.serialize(table2)
+		local jstr3 = json.serialize(table3)
+		local jstr4 = json.serialize(table4)
+
+		assert.is_true(json.isNull(json.null()))
+		assert.is_false(json.isNull(nil))
+
+		assert.are.same('{"foo":"apple"}', jstr1)
+		assert.are.same('{"bar":1234.5}', jstr2)
+		assert.are.same('{"baz":null}', jstr3)
+
+		assert.are.same(table1, json.parse(jstr1))
+		assert.are.same(table2, json.parse(jstr2))
+		assert.are.same(table3, json.parse(jstr3))
+		assert.are.same(table4, json.parse(jstr4))
 	end)
 end)
